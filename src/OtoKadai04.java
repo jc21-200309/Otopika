8c0d546c4b5e94efc6152246d7ea1c687b4d050c
200309 <200309@e0601-26.JC-21.local>
2022-01-27 12:47:29 +0900

    課題の提出です。

---
+++ src/OtoKadai04.java
@@ -0,0 +1,33 @@
+import jp.jc21.t.yoshizawa.otopika.Otopika;
+
+/**
+ *
+ */
+
+
+/**
+ * @author @AUTHOR AB2B18
+ *
+ */
+public class OtoKadai04  {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Otopika oto1 = new Otopika();
+		Otopika oto2 = new Otopika();
+		oto1.powerOn();
+		oto2.powerOn();
+		while(true) {
+			oto1.lightAll();
+			oto1.sleep();
+			oto1.lightOff();
+			oto2.lightAll();
+			oto2.sleep();
+			oto2.lightOff();
+		}
+
+	}
+
+}
